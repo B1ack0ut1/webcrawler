cd7c45caec0084f761e2e1911efe7482f08875e9
B1ack0ut1 <42845068+B1ack0ut1@users.noreply.github.com>
2023-11-23 23:24:16 +0800

    Print crawl report finished

---
+++ report.test.js
@@ -0,0 +1,34 @@
+const { test, expect } = require("@jest/globals");
+const { sortPages } = require("./report.js");
+
+test("sortPages 2 pages", () => {
+  const input = {
+    "https://blog.boot.dev/path": 1,
+    "https://blog.boot.dev": 3,
+  };
+  const actual = sortPages(input);
+  const expected = [
+    ["https://blog.boot.dev", 3],
+    ["https://blog.boot.dev/path", 1],
+  ];
+  expect(actual).toEqual(expected);
+});
+
+test("sortPages 5 pages", () => {
+  const input = {
+    "https://blog.boot.dev/path": 1,
+    "https://blog.boot.dev": 3,
+    "https://blog.boot.dev/path2": 5,
+    "https://blog.boot.dev/path3": 2,
+    "https://blog.boot.dev/path4": 9,
+  };
+  const actual = sortPages(input);
+  const expected = [
+    ["https://blog.boot.dev/path4", 9],
+    ["https://blog.boot.dev/path2", 5],
+    ["https://blog.boot.dev", 3],
+    ["https://blog.boot.dev/path3", 2],
+    ["https://blog.boot.dev/path", 1],
+  ];
+  expect(actual).toEqual(expected);
+});
